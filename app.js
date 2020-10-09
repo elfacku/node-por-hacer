1d67a72d717d95c57351007af76afd40f776de8e
Facku Kennel <facundo.kennel@gmail.com>
2020-10-09 10:26:31 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const colors = require('colors');
+
+const porHacer = require('./por-hacer/por-hacer');
+
+const argv = require('./config/yargs').argv;
+
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'crear':
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
+        break;
+
+    case 'listar':
+
+        let listado = porHacer.getListado();
+
+        for (let tarea of listado) {
+            console.log('======== Por Hacer ======='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log('=========================='.green);
+
+        }
+
+        break;
+
+    case 'actualizar':
+
+        let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
+        console.log(actualizado);
+        break;
+
+    case 'borrar':
+        let borado = porHacer.borrar(argv.descripcion);
+        console.log(borado);
+        break;
+
+    default:
+        console.log('comando no reconocido');
+
+}
